f4745d1b724339e01e17c6e995fae1e690dfb157
Bryan Tsai <bryan@bryantsai.com>
2014-10-20 22:18:31 +0800

    Fix script 'create-inst.sh' not properly returned.

--- create-inst.sh
+++ create-inst.sh
@@ -8,4 +8,5 @@ docker run --rm=true -i --volumes-from=db2_data_$inst bryantsai/db2-expc /bin/ba
 userdel dasusr1;userdel db2fenc1;userdel db2inst1;groupdel dasadm1;groupdel db2fgrp1;groupdel db2grp1
 groupadd db2grp1;groupadd db2fgrp1;groupadd dasadm1;useradd -g db2grp1 -m -d /home/db2inst1 db2inst1 -p db2inst1;useradd -g db2fgrp1 -m -d /home/db2fenc1 db2fenc1 -p db2fenc1;useradd -g dasadm1 -m -d /home/dasusr1 dasusr1 -p dasusr1
 /opt/ibm/db2/V10.5/instance/db2icrt -p $port -u db2fenc1 db2inst1
+exit
 EOF
